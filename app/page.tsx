b594d66b9a36bf8a42f4b0045272ba04f17e7ae3
Kofiastro <astrokofi@gmail.com>
2025-03-02 19:45:39 +0000

    container2 revamp

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,6 @@ import { Charts } from "./components/charts";
 import { SiGithub,SiLinkedin,SiYoutube  } from "react-icons/si";
 
 
-
-
-
-
 export default function Home() {
   return (
     <div className="relative scroll-smooth">
@@ -27,17 +23,16 @@ export default function Home() {
       height={10}
       alt="Vercel logo"
     />
-      
-   <NavVercel/>
+    <NavVercel/>
    {/* Buttons section */}
-     </div>
+      </div>
         <div className="space-x-4 hidden my-2 lg:flex *:lg:px-6  *:text-sm *:border-1 *:border-slate-50/10">
           <button className="rounded-md  hover:dark:bg-stone-900  ">Log in</button>
           <button className="rounded-md  hover:dark:bg-stone-900  ">Contact</button>
           <button className="rounded-md  bg-slate-950 dark:bg-stone-100 dark:text-black text-white hover:dark:bg-gray-200">Sign up</button>
         </div>
         <div className="flex items-center  lg:hidden justify-center">
-       <Menu03Icon className="lg:hidden flex size-8"/>
+        <Menu03Icon className="lg:hidden flex size-8"/>
         </div>
     </nav>
     {/* Homepage */}
@@ -58,22 +53,32 @@ export default function Home() {
           </h1>
           <h1 className="text-xl lg:text-3xl font-semibold items-center inline-block">Launch globally instantly <span><Globe02Icon className="inline-block size-6 align-middle"/> </span>Keep pushing<span className="inline-block align-middle size-6"><GitMergeIcon/> </span></h1>
       </div>
-       </div>
+        </div>
        {/* Gap */}
-       <div className="border-1 border-t-0 border-b-0 border-slate-50/10 py-2">
+        <div className="border-1 border-t-0 border-b-0 border-slate-50/10 py-2">
       </div>
        {/* container 2  */}
-       <div className="grid grid-row-1 md:grid-cols-2  md:divide-x md:divide-y-0  divide-y divide-slate-50/10 border-1 border-slate-50/10 ">
+        <div className="grid grid-row-1 relative md:grid-cols-2  md:divide-x md:divide-y-0  divide-y divide-slate-50/10 border-1 border-slate-50/10 ">
        {/* First Column */}
-       <div className="flex flex-col space-y-3 p-10 select-none">
+        <div className="flex flex-col space-y-3 p-10 select-none">
         <h1 className="text-stone-400 text-lg items-center flex"><span className="pr-2"><TerminalIcon className="size-6"/></span>Git-connected Deploys</h1>
         <div className="flex flex-col">
         <h1 className="text-2xl font-medium">From localhost to https, in seconds.</h1> 
         <h1 className="text-stone-400 text-2xl font-medium">Deploy from Git or from your Cli</h1>
         </div>
         <div className="relative">
-          <div className="p-2 relative ring-1 ring-gray-50/10  rounded-md max-w-xs ">
-          <div >
+        <div className="relative border-1  bg-black p-4 max-w-xs rounded-md hidden  md:flex font-medium border-gray-50/10">
+         <ul className="text-left text-xs font-mono text-stone-300 space-y-1 "><li className="flex items-center"> <span className="pr-2"><IoTriangleSharp/></span>git push -u origin main </li>
+         <li>  Enumerating objects: 7, done.</li>
+         <li> Counting objects: 100% (7/7), done.</li>
+         <li>Delta compression using up to 12 threads</li>
+         <li>Compressing objects: 100% (4/4), done.</li>
+         <li> Writing objects: 100% (4/4), 412 bytes | 412.00 KiB/s, done.</li>
+         </ul>
+        
+          </div>
+          <div className=" relative md:absolute md:left-30 md:-bottom-32 bg-black p-2 ring-1 ring-gray-50/10  rounded-md max-w-xs ">
+          <div>
             <div className="space-x-1">
             <button className="p-1 rounded-full bg-red-500"></button>
             <button className="p-1 rounded-full bg-blue-500"></button>
@@ -82,12 +87,16 @@ export default function Home() {
             <div className=" text-xs font-medium ring-1 px-6 py-1 my-2 rounded-md ring-gray-50/10
             "><h1>Vercel.com</h1></div>
           </div>
-          <h1 className=" text-xl md:text-3xl flex font-medium p-10  rounded-md border-1 border-gray-50/10 ">What will you ship ? <span><IoTriangleSharp className="size-20 opacity-20"/></span></h1>
+            <div className=" p-5 border-1 border-gray-50/10 ">
+          <h1  className=" text-lg md:text-2xl font-medium text-gray-50 bg-clip-text rounded-md ">What will you ship ?</h1>
+          <div className=" flex items-center justify-center"><IoTriangleSharp className="size-20 md:size-10 opacity-20"/></div>
+            </div>
           </div>
+          
         </div>
-       </div>
+        </div>
        {/* Second COlumn */}
-     <div className="flex flex-col space-y-3 p-10 select-none">
+      <div className="flex flex-col space-y-3 p-10 select-none">
         <h1 className="text-stone-400 text-lg items-center flex"><span className="pr-2"><NotificationBubbleIcon className="size-5"/></span>Collaborative pre-production
         </h1>
         <div className="flex flex-col">
@@ -107,24 +116,24 @@ export default function Home() {
       <h1>This looks great </h1>
       <CircleArrowUp02Icon className="size-5 text-white"/>
         </div>
-       </div>
-       </div>
+        </div>
+        </div>
        {/* Container  3 */}
       <div className=" border-1 border-slate-50/10  border-t-0 border-b-0  p-10">
       {/* First Column */}
-        <div className="flex flex-col space-y-3 relative">
+        <div className="flex flex-col space-y-3 ">
         <h1 className="text-stone-400 text-lg items-center flex"><span className="pr-2"><ChartLineData01Icon className="size-5"/></span>Front-end Observability</h1>
         <div className="flex flex-col">
         <h1 className=" text-2xl font-medium">Privacy-friendly,lightweight Analytics.</h1> 
         <h1 className="text-stone-400 text-2xl font-medium md:w-150">Upgrade your post-launch workflow with actionable, real insights.</h1>
         </div>
-       </div>
-       <Charts/>
-       </div>
-       {/* Container 4 */}
-       <div className="grid grid-row-1 md:grid-cols-2 select-none md:divide-x md:divide-y-0 divide-y divide-slate-50/10 border-1 border-b-0 border-slate-50/10 ">
+        <Charts/>
+        </div>
+          </div>
+        {/* Container 4 */}
+        <div className="grid grid-row-1 md:grid-cols-2 select-none md:divide-x md:divide-y-0 divide-y divide-slate-50/10 border-1 border-b-0 border-slate-50/10 ">
        {/* First Column */}
-       <div className="flex flex-col space-y-4 p-10">
+        <div className="flex flex-col space-y-4 p-10">
         <h1 className="text-stone-400 text-lg items-center flex "><span className="pr-2"><ReplayIcon className="size-5"/></span>Instant Rollbacks
         </h1>
         <div className="flex flex-col">
@@ -144,9 +153,7 @@ export default function Home() {
       <h1 className="text-stone-400">Update bento grid design</h1>
         </div>
         </div>
-     
         <FaArrowsRotate className="size-15 opacity-40"/>
-
         <div className="ring-1 ring-gray-50/10 rounded-md p-3 max-w-sm ">
         <div className="text-sm font-medium flex justify-between items-center">
       <h1>Vecrcel-site/ <span className="text-stone-400">Jbx24yC</span> </h1>
@@ -160,34 +167,34 @@ export default function Home() {
       <h1 className="text-stone-400">Update bento grid design</h1>
         </div>
         </div>
-       </div>
+        </div>
        {/* Second COlumn */}
-     <div className="flex flex-col space-y-4 p-10">
+      <div className="flex flex-col space-y-4 p-10">
         <h1 className="text-stone-400 text-lg items-center flex"><span className="pr-2"><Book01Icon className="size-5"/></span>Conformance
         </h1>
         <div className="flex flex-col">
         <h1 className="text-2xl font-medium ">Move fast, don’t break things.<span className="text-stone-400"> Keep quality high while maintaining velocity with Enterprise Monorepos.</span>.</h1> 
         </div>
         <div className="grid grid-cols-2 ring-1 ring-gray-50/10 ">
-         <div className="p-3 space-y-3">
+          <div className="p-3 space-y-3">
           <h1>Conformance</h1>
             <div className="ring-1 ring-gray-50/10 rounded-md p-6 max-w-sm bg-slate-50/5 flex items-center justify-between">
             <h1 className=" text-xs md:text-sm">Excellent</h1>
             <FaCircleNotch className=" size-4 md:size-6 text-emerald-400"/>
             <h1 className="text-sm ">9.4</h1>
-             </div>
+              </div>
             <div className="ring-1 ring-gray-50/10 rounded-md  md:text-sm p-6 max-w-sm text-xs  bg-slate-50/5">Total Issues</div>
             <div className="ring-1 ring-gray-50/10 rounded-md md:text-sm p-6 max-w-sm  text-xs bg-slate-50/5">Major Issues</div>
-         </div>
-         <div className="p-3 space-y-3">
+          </div>
+          <div className="p-3 space-y-3">
           <h1>Code Owners</h1>
             <div className="ring-1 ring-gray-50/10 rounded-md p-6 max-w-sm text-xs  md:text-sm bg-slate-50/5">Vercel/design</div>
             <div className="ring-1 ring-gray-50/10 rounded-md p-6 max-w-sm text-xs md:text-sm bg-slate-50/5">Vercel/eng</div>
             <div className="ring-1 ring-gray-50/10 rounded-md p-6 max-w-sm text-xs  md:text-sm bg-slate-50/5">Vercel/marketing</div>
-         </div>
+          </div>
         </div>
-       </div>
-       </div>
+        </div>
+        </div>
        {/* container 5 */}
       <div className=" border-1 border-slate-50/10 p-10 flex flex-col space-y-2 md:space-y-0 md:flex-row items-center  justify-center">
       <div className="flex items-baseline">
@@ -198,15 +205,14 @@ export default function Home() {
       <h1 className="text-xl lg:text-2xl font-medium">Without compromising </h1>
       <button className="mx-2 hover:dark:bg-stone-900 p-2 border-1 border-slate-50/10 rounded-full text-white flex hover:bg-gray-300  text-sm items-center"><span className="px-2"><Shield02Icon className="size-4"/></span>Security</button>
       </div>
-      
-       </div>
+        </div>
        {/* Gap 2 */}
-       <div className="border-1 border-t-0 border-b-0 border-slate-50/10 py-2">
+        <div className="border-1 border-t-0 border-b-0 border-slate-50/10 py-2">
       </div>
       {/* Container 6 */}
       <div className="flex flex-col md:flex-row  divide-x divide-dashed divide-slate-50/10 border-1 border-slate-50/10 ">
        {/* First Column */}
-         <div className=" space-y-4 p-10 text-center  md:text-left  ">
+          <div className=" space-y-4 p-10 text-center  md:text-left  ">
         <div className="flex flex-col ">
         <h1 className="text-2xl font-medium ">Ready to deploy? <span className="text-stone-400"> Start building with a free account. Speak to an expert for your <span className="text-blue-400">Pro</span> or <span className="text-purple-400">Enterprise</span> needs.</span></h1> 
         </div>
@@ -214,18 +220,17 @@ export default function Home() {
         <button className=" hover:dark:bg-stone-200 p-2 bg-white border-1 border-slate-50/10 rounded-full text-black flex  text-sm items-center">Start Deploying</button>
         <button className=" hover:dark:bg-stone-900 p-2 border-1 border-slate-50/10 rounded-full text-white flex hover:bg-gray-300  text-sm items-center">Talk to an Expert</button>
         </div>
-       </div>
+        </div>
        {/* Second COlumn */}
-       <div className="space-y-4 p-10 max-w-xs">
+        <div className="space-y-4 p-10 max-w-xs">
         <div className="flex flex-col">
         <h1 className="text-base font-medium ">Explore Vercel Enterprise <span className="text-stone-400"> with an interactive product tour, trial, or a personalized demo.</span></h1> 
         </div>
         <div className="font-medium">
         <button className=" hover:dark:bg-stone-900 p-2 border-1 border-slate-50/10 rounded-full text-white flex hover:bg-gray-300  text-sm items-center">Explore Enterprise</button>
         </div>
-       </div>
-       </div>
-       
+        </div>
+        </div>
     </main>
     {/* Footer */}
     <footer className="lg:mx-65 mx-5">
@@ -287,11 +292,8 @@ export default function Home() {
       <div className="flex justify-between mb-10">
         <button className="text-blue-500 text-sm hover:bg-gray-50/10 font-medium p-2 rounded-md hover:border-gray-50/10"> All Systems normal</button>
       </div>
-    
-     </footer>
+      </footer>
     </div>
     </div>  
-
-    
   );
 }
